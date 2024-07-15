1aa00dceab081e1f07057facebe0a1b155ceaf2d
eLHa_ <Muhammadelha13@gmail.com>
2024-07-15 19:04:24 +0700

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import requests
 
 def send_sms(phone_number, text):
@@ -17,43 +16,20 @@ def send_sms(phone_number, text):
 
     try:
         response = requests.post(url, json=payload, headers=headers)
-        if response.status_code == 200:
-            return True
-        else:
-            print(f"Failed to send SMS to {phone_number}. Status code: {response.status_code}")
-            return False
+        print(response.json())  # Cetak respons dari API
     except Exception as e:
         print(f"Error occurred: {str(e)}")
-        return False
 
-def main(cnt, phone_number, text):
+if __name__ == '__main__':
     try:
         print("""\033[1m
    _____ __  ________
-  / ___//  |/  / ___/ | SMS Spammer
-  \__ \/ /|_/ /\__ \  | SMS Spammer using Rapid API
+  / ___//  |/  / ___/ | SMS Sender
+  \__ \/ /|_/ /\__ \  | SMS Sender using Rapid API
  ___/ / /  / /___/ /  | Coded by Mrxpoint
 /____/_/  /_//____/   | ex: +15684036121\033[0m
         """)
 
-        success_count = 0
-        for i in range(cnt):
-            success = send_sms(phone_number, text)
-            if success:
-                success_count += 1
-                print(f"[{success_count}] SMS sent to {phone_number} successfully.")
-            else:
-                print(f"[{i + 1}] Failed to send SMS to {phone_number}")
-            
-            # Tambahkan penundaan 1 detik antara pengiriman SMS
-            time.sleep(1)
-
-    except (KeyboardInterrupt, EOFError):
-        print("\n")
-        sys.exit()
-
-if __name__ == '__main__':
-    try:
         phone_number = input("No    : ").strip()
         if not phone_number.startswith('+') or not phone_number[1:].isdigit():
             print("Check your phone number format! It should start with '+' and contain only digits.")
@@ -64,18 +40,8 @@ if __name__ == '__main__':
             print("Text should not be empty!")
             sys.exit()
 
-        cnt = input("Count : ").strip()
-        if not cnt.isdigit():
-            print("Check your count! It should be a number.")
-            sys.exit()
-
-        cnt = int(cnt)
-        if cnt <= 0:
-            print("Count should be greater than zero!")
-            sys.exit()
-
         print("")
-        main(cnt, phone_number, text)
+        send_sms(phone_number, text)
 
     except (KeyboardInterrupt, EOFError):
         print("\n")
